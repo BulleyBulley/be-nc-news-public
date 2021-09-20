7f6150e15af0e0accacda16c3dcfa4f9df558e1e
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-09-20 16:42:14 +0100

    adds seeded tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,5 +1,10 @@
 const db = require('../connection')
-//const format = require("pg-format")
+const format = require("pg-format");
+const { formatTopicsData, formatUsersData, formatArticlesData, formatCommentsData, createSlugRef } = require ("../utils/data-manipulation.js")
+
+
+
+//const promise1 = []
 
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
@@ -22,16 +27,16 @@ const seed = (data) => {
   .then(() => {
     return db.query(`
     CREATE TABLE topics (
-      slug SERIAL PRIMARY KEY,
+      slug VARCHAR(100) PRIMARY KEY,
       description VARCHAR(500)
     );`)
   })
   .then(() => {
     return db.query(`
     CREATE TABLE users (
-      username VARCHAR(50) PRIMARY KEY,
+      username VARCHAR(100) PRIMARY KEY,
       avatar_url VARCHAR(300),
-      name VARCHAR(200) NOT NULL
+      name TEXT NOT NULL
     );`)
   })
   .then(() => {
@@ -41,9 +46,9 @@ const seed = (data) => {
       title VARCHAR(200) NOT NULL,
       body TEXT NOT NULL,
       votes INT DEFAULT 0,
-      topic INT REFERENCES topics(slug) ON DELETE CASCADE,
-      author VARCHAR(50) REFERENCES users(username) ON DELETE CASCADE,
-      created_at TIMESTAMP
+      topic VARCHAR(200) REFERENCES topics(slug) ON DELETE CASCADE,
+      author VARCHAR(100) REFERENCES users(username) ON DELETE CASCADE,
+      created_at TEXT
    );`) 
   })
   .then (() => {
@@ -53,13 +58,71 @@ const seed = (data) => {
       author VARCHAR(100) REFERENCES users(username) ON DELETE CASCADE,
       article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
       votes INT DEFAULT 0,
-      created_at TIMESTAMP,
-      body VARCHAR(500)
+      created_at DATE,
+      body TEXT
     );`)
   })
   .then(() => {
     console.log('All tables created')
   })
+  .then (() => {
+    const queryStr = format (
+    `INSERT INTO topics
+    (slug, description)
+    VALUES
+    %L
+    RETURNING *;
+    `,
+    formatTopicsData(topicData)
+    );
+    return db.query(queryStr) 
+  })
+  .then(() => {
+    const queryStr = format (
+      ` INSERT INTO users
+      (username, avatar_url, name)
+      VALUES
+      %L
+      RETURNING *;
+      `,
+      formatUsersData(userData)
+    );
+    return db.query(queryStr)
+  })
+  .then (() => {
+    //console.log(topicData.rows)
+    //const slugRef = createSlugRef(result)
+    const queryStr = format (
+    `
+    INSERT INTO articles
+    (title, topic, author, body, created_at, votes)
+    VALUES
+    %L
+    RETURNING *;
+    `,
+    formatArticlesData(articleData)
+  );
+  return db.query(queryStr)  
+  })
+  .then (() => {
+  const queryStr = format (
+    `
+    INSERT INTO comments
+    (body, votes, author, article_id, created_at)
+    VALUES
+    %L
+    RETURNING *;
+    `,
+    formatCommentsData(commentData)
+  )  
+  return db.query(queryStr) 
+  })
+  .then (() => {
+    console.log('All tables seeded')
+      })
+
+  
+
   
 
 
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,1 +1,39 @@
 // extract any functions you are using to manipulate your data, into this file
+
+exports.formatTopicsData = (topicData) => {
+    const formattedTopicsData = topicData.map((topic) => {
+    return [topic.slug, topic.description];    
+    });
+    return formattedTopicsData;
+}
+
+exports.formatUsersData = (userData) => {
+    const formattedUsersData = userData.map((user) => {
+    return [user.username, user.avatar_url, user.name];    
+    });
+    return formattedUsersData;
+}
+
+exports.formatArticlesData = (articleData) => {
+    const formattedArticlesData = articleData.map((article) => {
+        return [article.title, article.topic, article.author, article.body, article.create_at, article.votes]; 
+    })
+    return formattedArticlesData;
+}
+
+exports.formatCommentsData = (commentData) => {
+    const formattedCommentsData = commentData.map((comment) => {
+        return [comment.body, comment.votes, comment.author, comment.article_id, comment.create_at];
+        
+    })
+    return formattedCommentsData;
+}
+
+// exports.createSlugRef = (topicsRows) => {
+//     console.log(topicsRows)
+//     const slugRef = {};
+//     topicsRows.forEach((topic) => {
+//         slugRef[topic.slug] = topic.topic
+//     })
+//     return slugRef
+//}
