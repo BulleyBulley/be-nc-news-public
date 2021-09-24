93aae7e9a641f8f87697b96885415d14382f650e
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-09-24 11:06:31 +0100

    updates endpoints for GET users

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -4,7 +4,7 @@ exports.getUsers = async (req, res, next) => {
     try {
       const allUsers = await fetchUsers();
       res.status(200).send({ allUsers });
-      console.log(allUsers)
+      //console.log(allUsers)
     } catch (err) {
       next(err);
     }
