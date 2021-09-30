082b5275a8cdad86ad00b2973b188dd8efc1ae54
BulleyBulley <philbulleyment@yahoo.co.uk>
2021-09-30 10:09:05 +0100

    updates articles models to async

--- models/articles-model.js
+++ models/articles-model.js
@@ -1,5 +1,8 @@
 const db = require("../db/connection");
-const { checkSortByExists,checkOrderExists } = require("../db/utils/data-manipulation");
+const {
+  checkSortByExists,
+  checkOrderExists,
+} = require("../db/utils/data-manipulation");
 
 exports.fetchArticle = async (article_id) => {
   const result = await db.query(
@@ -13,64 +16,55 @@ exports.fetchArticle = async (article_id) => {
   if (result.rows.length === 0) {
     return Promise.reject({ status: 404, msg: "Article Not Found" });
   }
-    return result.rows[0];
+  return result.rows[0];
 };
 
-exports.updateArticleVotesById = (article_id, patchVotesInfo) => {
-  return db
-    .query(
-      `UPDATE articles SET votes = $1 + votes WHERE article_id = $2 RETURNING *;`,
-      [patchVotesInfo, article_id]
-    )
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Article Not Found" });
-      }
-      if (result.rows[0].votes === null) {
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-      }
-      return result.rows;
-    });
+exports.updateArticleVotesById = async (article_id, patchVotesInfo) => {
+  const result = await db.query(
+    `UPDATE articles SET votes = $1 + votes WHERE article_id = $2 RETURNING *;`,
+    [patchVotesInfo, article_id]
+  );
+
+  if (result.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Article Not Found" });
+  }
+  if (result.rows[0].votes === null) {
+    return Promise.reject({ status: 400, msg: "Bad Request" });
+  }
+  return result.rows;
 };
 
 exports.fetchAllArticles = async (sort_by, order, topic, limit = 10, p = 1) => {
   const offset = (p - 1) * limit;
   const queryValues = [limit, offset];
-  const checkedSortBy = await checkSortByExists(sort_by)
-  const checkedOrder = await checkOrderExists(order)
+  const checkedSortBy = await checkSortByExists(sort_by);
+  const checkedOrder = await checkOrderExists(order);
   let queryStr = `
   SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, COUNT(comment_id) 
   AS comment_count
   FROM articles
   LEFT JOIN comments ON articles.article_id = comments.article_id
   GROUP BY articles.article_id`;
- 
+
   if (topic) {
     queryValues.push(topic);
     queryStr += ` HAVING articles.topic = $3`;
   }
-  
-  queryStr += ` ORDER BY ${checkedSortBy} ${checkedOrder} LIMIT $1 OFFSET $2;`; 
+
+  queryStr += ` ORDER BY ${checkedSortBy} ${checkedOrder} LIMIT $1 OFFSET $2;`;
   const result = await db.query(queryStr, queryValues);
   return result.rows;
   //add total count property
 };
 
-exports.updateArticleBodyByArticleId = (article_id, patchInfo) => {
-  return db
-    .query(
-      `UPDATE articles SET body = $1  
+exports.updateArticleBodyByArticleId = async (article_id, patchInfo) => {
+  const result = await db.query(
+    `UPDATE articles SET body = $1  
       WHERE article_id = $2 RETURNING *;`,
-      [patchInfo, article_id]
-    )
-    .then((result) => {
-      if (result.rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Article Not Found" });
-      }
-      if (result.rows[0].votes === null) {
-        return Promise.reject({ status: 400, msg: "Bad Request" });
-      }
-      //console.log(result.rows)
-      return result.rows;
-    });
+    [patchInfo, article_id]
+  );
+  if (result.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "Article Not Found" });
+  }
+  return result.rows;
 };
